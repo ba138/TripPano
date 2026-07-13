b1e2f06749e50a3c0426805a3db7656c89ed4016
ba138 <basitalyshah51214@gmail.com>
2026-07-13 14:02:52 +0500

    Initial Commit

---
+++ TripPano/TripPanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPanoApp.swift
+//  TripPano
+//
+//  Created by Basit Ali on 13/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TripPanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
