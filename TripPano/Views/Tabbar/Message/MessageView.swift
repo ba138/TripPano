95c71e1a1240212af9781ae369bb541cf8d2be5f
ba138 <basitalyshah51214@gmail.com>
2026-08-02 20:24:41 +0500

    fix issues of the ui in message view

--- TripPano/Views/Tabbar/Message/MessageView.swift
+++ TripPano/Views/Tabbar/Message/MessageView.swift
@@ -9,51 +9,51 @@ import SwiftUI
 
 struct MessageView: View {
     var body: some View {
+        ScrollView(showsIndicators : false){
+            VStack{
+               
+                HStack{
+                    Image("sublogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                    Spacer()
+                    
+                    NavigationLink {
+                       NotificationView()
+                    } label: {
+                        Circle()
+                            .frame(width: 40,height: 40)
+                            .foregroundColor(AppColors.subTitle.opacity(0.3))
+                            .overlay {
+                                Image(systemName: "bell")
+                            }
+                    }
 
-        VStack{
-           
-            HStack{
-                Image("sublogo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 80)
-                Spacer()
-                
-                NavigationLink {
-                   NotificationView()
-                } label: {
-                    Circle()
-                        .frame(width: 40,height: 40)
-                        .foregroundColor(AppColors.subTitle.opacity(0.3))
-                        .overlay {
-                            Image(systemName: "bell")
-                        }
+                    
                 }
-
+                Text("Message")
+                    .font(.system(size: 20))
+                    .bold()
+                    .foregroundColor(AppColors.fontTitle)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                Text("Your conversations are backed up here")
+                    .font(.system(size: 16))
+                    .foregroundColor(AppColors.subTitle)
+                    .frame(maxWidth: .infinity,alignment: .leading)
                 
-            }
-            Text("Message")
-                .font(.system(size: 20))
-                .bold()
-                .foregroundColor(AppColors.fontTitle)
-                .frame(maxWidth: .infinity,alignment: .leading)
-            Text("Your conversations are backed up here")
-                .font(.system(size: 16))
-                .foregroundColor(AppColors.subTitle)
-                .frame(maxWidth: .infinity,alignment: .leading)
-            ScrollView(showsIndicators : false) {
+                
                 ForEach(0..<10, id: \.self) { _ in
                     MessageCard()
                 }
             }
-            Spacer()
-            
+            .padding(.horizontal, 20)
+
+            .ignoresSafeArea()
+            .toolbar(.hidden, for: .navigationBar)
+
         }
-        .padding(20)
-
-        .ignoresSafeArea()
-        .toolbar(.hidden, for: .navigationBar)
-
+        
     }
 }
 
