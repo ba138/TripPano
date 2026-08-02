81ccf94e8c1b6a2b570ce5940bd919d02c15340d
ba138 <basitalyshah51214@gmail.com>
2026-08-02 20:18:00 +0500

    for loop to message view for message card

--- TripPano/Views/Tabbar/Message/MessageView.swift
+++ TripPano/Views/Tabbar/Message/MessageView.swift
@@ -11,10 +11,40 @@ struct MessageView: View {
     var body: some View {
 
         VStack{
-MessageCard()
+           
+            HStack{
+                Image("sublogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 80)
+                Spacer()
+                
+                NavigationLink {
+                   NotificationView()
+                } label: {
+                    Circle()
+                        .frame(width: 40,height: 40)
+                        .foregroundColor(AppColors.subTitle.opacity(0.3))
+                        .overlay {
+                            Image(systemName: "bell")
+                        }
+                }
+
+                
+            }.padding(.vertical,20)
+            ScrollView(showsIndicators : false) {
+                ForEach(0..<10, id: \.self) { _ in
+                    MessageCard()
+                }
+            }
+            Spacer()
             
         }
-        .padding()
+        .padding(.horizontal,20)
+
+        .ignoresSafeArea()
+        .toolbar(.hidden, for: .navigationBar)
+
     }
 }
 
